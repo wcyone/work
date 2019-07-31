88c7ba296e52b97441dec4ab3ca2cd2f85e2f88e
wcyone <421035835@qq.com>
2019-07-31 18:02:44 +0800

    es

--- src/main/java/com/sccdrs/work/controller/LoginController.java
+++ src/main/java/com/sccdrs/work/controller/LoginController.java
@@ -1,10 +1,12 @@
 package com.sccdrs.work.controller;
 
 import com.sccdrs.work.service.UserService;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.transport.TransportClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
